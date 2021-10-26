52a25f04c23f21c40ad2801825af8782554a8583
Daniya Niazi <farihadania@hotmail.com>
2021-10-26 19:42:17 +0500

    mutation and refetch

--- grahql_frontend/src/components/DisplaData.jsx
+++ grahql_frontend/src/components/DisplaData.jsx
@@ -1,4 +1,4 @@
-import { useQuery, gql, useLazyQuery } from "@apollo/client";
+import { useQuery, gql, useLazyQuery, useMutation } from "@apollo/client";
 import React, { useState } from "react";
 const QUERY_ALL_USERS = gql`
   query getAllUsers {
@@ -29,10 +29,26 @@ const GET_MOVIE_BY_NAME = gql`
     }
   }
 `;
-const DisplaData = () => {
-  const [movieSearched, setMovieSearched] = useState("");
 
-  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+const CREATE_USER_MUTATION = gql`
+  mutation ($input: CreateUserInput!) {
+    createUser(input: $input) {
+      id
+      name
+      age
+    }
+  }
+`;
+
+const DisplaData = () => {
+  const [user, setUser] = useState({
+    name: "",
+    age: "",
+    username: "",
+    nationality: "",
+  });
+  const [movieSearched, setMovieSearched] = useState("");
+  const { data, loading, error, refetch } = useQuery(QUERY_ALL_USERS);
   const {
     data: movies,
     loading: movieLoading,
@@ -40,24 +56,89 @@ const DisplaData = () => {
   } = useQuery(QUERY_ALL_MOVIES);
   const [FetchMovieByName, { data: searchedMovie, error: searchError }] =
     useLazyQuery(GET_MOVIE_BY_NAME);
-  console.log(data);
+  const [createUser] = useMutation(CREATE_USER_MUTATION);
   if (loading || movieLoading) return "Loading ... ";
   if (error || movieErrors || searchError) {
     console.log(searchError);
   }
-
+  const handleChange = (e) => {
+    console.log(user);
+    console.log(e.target.name, e.target.value);
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div>
-      <h1>LIST OF USER</h1>
-      {data &&
-        data.Users.map((user) => {
-          return <div>{user.name}</div>;
-        })}
-      <h1>LIST OF Movies</h1>
-      {data &&
-        movies.Movies.map((movie) => {
-          return <div>{movie.name}</div>;
-        })}
+      <div className="section">
+        <div>
+          <h1>LIST OF USER</h1>
+          {data &&
+            data.Users.map((user) => {
+              return <div>{user.name}</div>;
+            })}
+        </div>
+        <div>
+          <h1>CREATE USER</h1>
+          <input
+            type="text"
+            placeholder="name"
+            name="name"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="age"
+            name="age"
+            onChange={handleChange}
+          />
+          <select
+            name="nationality"
+            value={user.nationality}
+            onChange={handleChange}
+          >
+            <option value="BRAZIL">BRAZIL</option>
+            <option value="CANADA">CANADA</option>
+            <option value="INDIA">INDIA</option>
+            <option value="GERMANY">GERMANY</option>
+            <option value="CHILE">CHILE</option>
+          </select>
+          <br />
+          <button
+            onClick={async () => {
+              await createUser({
+                variables: {
+                  input: {
+                    name: user.name,
+                    username: user.username,
+                    age: Number(user.age),
+                    nationality: user.nationality,
+                  },
+                },
+              });
+              refetch();
+            }}
+          >
+            Create User
+          </button>
+        </div>
+      </div>
+      <div className="section">
+        <div>
+          <h1>LIST OF Movies</h1>
+          {data &&
+            movies.Movies.map((movie) => {
+              return <div>{movie.name}</div>;
+            })}
+        </div>
+      </div>
       <div>
         <input
           type="text"
@@ -80,8 +161,8 @@ const DisplaData = () => {
             {searchedMovie.Movie.name} -{searchedMovie.Movie.yearOfPublication}
           </h3>
         )}
+        {searchError && <div>Movie searched Error</div>}
       </div>
-      {searchError && <div>Movie searched Error</div>}
     </div>
   );
 };
